ec6552853edde5f64a18a6fbb9a1fe7752d79ddc
Documenter.jl <documenter@juliadocs.github.io>
2022-06-23 21:38:44 +0000

    build based on a431c86

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ConsoleLayoutGenerator","category":"page"},{"location":"#ConsoleLayoutGenerator","page":"Home","title":"ConsoleLayoutGenerator","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ConsoleLayoutGenerator.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ConsoleLayoutGenerator]","category":"page"}]
+}
